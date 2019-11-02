87197a6e7ba55ac0e664713959590fa3d23898f7
MacVP <48820988+MacVP@users.noreply.github.com>
2019-11-02 18:25:48 +0800

    Update

---
+++ GDS220_Meaningful_Game/Assets/Scripts/InteractableManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableManager : MonoBehaviour
+{
+    public GameObject berries;
+    public bool lookedAt;
+    public bool berryBPicked = false;
+
+    void Update()
+    {
+        PickBerry();
+    }
+
+    void PickBerry()
+    {
+        if(lookedAt == true)
+        {
+            Debug.Log("Picked Berries");
+            berries.SetActive(false);
+        }
+    }
+}
---
+++ GDS220_Meaningful_Game/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public float range = 3;
+    public LayerMask layerMask;
+    public Text text;
+    private InteractableManager lookingAt;
+
+    void Update()
+    {
+        BerryBush();
+    }
+
+    void BerryBush()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range, layerMask) && lookingAt.berryBPicked == false)
+        {
+            Debug.Log("Looking at Berry Bush");
+            text.enabled = true;
+
+            lookingAt = hit.collider.gameObject.GetComponent<InteractableManager>();
+
+            if (hit.collider.gameObject.GetComponent<InteractableManager>() != null && Input.GetButtonDown("Interact"))
+            {
+                hit.collider.gameObject.GetComponent<InteractableManager>().lookedAt = true;
+                hit.collider.gameObject.GetComponent<InteractableManager>().berryBPicked = true;
+                Debug.Log("working");
+            }
+            else
+            {
+                Debug.Log("No Script");               
+            }
+        }
+        else
+        {
+            text.enabled = false;
+        }
+    }
+}
