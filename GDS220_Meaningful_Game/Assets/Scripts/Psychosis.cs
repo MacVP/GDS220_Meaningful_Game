2db752e2f2e64c2f98fdd23c1febcd32ad102b08
MacVP <48820988+MacVP@users.noreply.github.com>
2019-11-11 16:41:35 +0800

    Update

---
+++ GDS220_Meaningful_Game/Assets/Scripts/Psychosis.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Psychosis : MonoBehaviour
+{
+    public GameObject[] voicePos;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        PsychosisTest();
+    }
+
+    void PsychosisTest()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            GameObject currentPos = voicePos [Random.Range(0, voicePos.Length)];
+            Debug.Log(currentPos);
+            AudioSource audio = currentPos.GetComponent<AudioSource>();
+            audio.Play();
+        }
+    }
+}
