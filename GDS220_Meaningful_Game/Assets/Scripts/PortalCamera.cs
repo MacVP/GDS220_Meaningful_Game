488669d3ec7c2cd0e58418e61159dff8cba57704
MacVP <48820988+MacVP@users.noreply.github.com>
2019-11-13 12:10:27 +0800

    Update

---
+++ GDS220_Meaningful_Game/Assets/Scripts/PortalCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //GetOffset();
+        GetRotation();
+    }
+
+    void GetOffset()
+    {
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position - playerOffsetFromPortal;
+    }
+
+    void GetRotation()
+    {
+        float angularDefferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+
+
+        Quaternion portalRotationalDefference = Quaternion.AngleAxis(angularDefferenceBetweenPortalRotations, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDefference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+}
