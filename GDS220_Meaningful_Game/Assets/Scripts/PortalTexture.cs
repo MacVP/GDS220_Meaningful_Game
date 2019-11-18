ee940b7431a8e25937cf96d71b5b20728a32c431
MacVP <48820988+MacVP@users.noreply.github.com>
2019-11-18 23:32:28 +0800

    Update

--- GDS220_Meaningful_Game/Assets/Scripts/PortalTexture.cs
+++ GDS220_Meaningful_Game/Assets/Scripts/PortalTexture.cs
@@ -4,22 +4,53 @@ using UnityEngine;
 
 public class PortalTexture : MonoBehaviour
 {
-    public Camera cameraB;
+    public Camera lvl1CamB;
+    public Camera redPlane;
+    public Camera pyreCamB;
 
-    public Material cameraMatB;
+    public Material lvl1MatB;
+    public Material redPlaneB;
+    public Material pyreMatB;
 
     void Start()
     {
-        if (cameraB.targetTexture != null)
-        {
-            cameraB.targetTexture.Release();
-        }
-        cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
-        cameraMatB.mainTexture = cameraB.targetTexture;
+        Lvl1();
+        RedPlane();
+        Pyre();
     }
 
     void Update()
     {
         
+    }
+
+    void Lvl1()
+    {
+        if (lvl1CamB.targetTexture != null)
+        {
+            lvl1CamB.targetTexture.Release();
+        }
+        lvl1CamB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        lvl1MatB.mainTexture = lvl1CamB.targetTexture;
+    }
+
+    void RedPlane()
+    {
+        if (redPlane.targetTexture != null)
+        {
+            redPlane.targetTexture.Release();
+        }
+        redPlane.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        redPlaneB.mainTexture = redPlane.targetTexture;
+    }
+
+    void Pyre()
+    {
+        if (pyreCamB.targetTexture != null)
+        {
+            pyreCamB.targetTexture.Release();
+        }
+        pyreCamB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        pyreMatB.mainTexture = pyreCamB.targetTexture;
     }
 }
