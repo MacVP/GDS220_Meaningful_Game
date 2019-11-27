516300e522957de0ab8be0949dc7bf984b468b19
MacVP <48820988+MacVP@users.noreply.github.com>
2019-11-27 17:15:25 +0800

    Update

--- GDS220_Meaningful_Game/Assets/Scripts/LevelOptimisation.cs
+++ GDS220_Meaningful_Game/Assets/Scripts/LevelOptimisation.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class LevelOptimisation : MonoBehaviour
 {
@@ -12,8 +13,8 @@ public class LevelOptimisation : MonoBehaviour
 
     private void Start()
     {
-        Level_1A.SetActive(false);
-        Level_1B.SetActive(false);
+        Level_1A.SetActive(true);
+        Level_1B.SetActive(true);
         Level_1C.SetActive(false);
         Level_1D.SetActive(false);
         Level_1E.SetActive(false);
@@ -23,45 +24,85 @@ public class LevelOptimisation : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("Player") && gameObject.name == "1A-1B")
+        if (other.CompareTag("Player") && gameObject.name == "1A-1B")
         {
-            Level_1A.SetActive(true);
-            Level_1B.SetActive(true);
+            Invoke("LevelShow_1", 0.01f);
+            return;
         }
 
         if (other.CompareTag("Player") && gameObject.name == "1B-1C")
         {
-            Level_1A.SetActive(false);
-            Level_1B.SetActive(true);
-            Level_1C.SetActive(true);
+            Invoke("LevelShow_2", 0.01f);
+            return;
         }
 
         if (other.CompareTag("Player") && gameObject.name == "1C-1D")
         {
-            Level_1B.SetActive(false);
-            Level_1C.SetActive(true);
-            Level_1D.SetActive(true);
+            Invoke("LevelShow_3", 0.01f);
+            return;
         }
 
         if (other.CompareTag("Player") && gameObject.name == "1D-1E")
         {
-            Level_1C.SetActive(false);
-            Level_1D.SetActive(true);
-            Level_1E.SetActive(true);
+            Invoke("LevelShow_4", 0.01f);
+            return;
         }
 
         if (other.CompareTag("Player") && gameObject.name == "1E-PyreA")
         {
-            Level_1D.SetActive(false);
-            Level_1E.SetActive(true);
-            Pyre_A.SetActive(true);
+            Invoke("LevelShow_5", 0.01f);
+            return;
         }
 
         if (other.CompareTag("Player") && gameObject.name == "PyreA-PyreB")
         {
-            Level_1E.SetActive(false);
-            Pyre_A.SetActive(true);
-            Pyre_B.SetActive(true);
+            Invoke("LevelShow_6", 0.01f);
+            return;
         }
+        else
+        {
+            Invoke("LevelShow_7", 0.01f);
+            return;
+        }
+    }
+
+    void LevelShow_1()
+    {
+        Level_1A.SetActive(true);
+        Level_1B.SetActive(true);
+    }
+    void LevelShow_2()
+    {
+        Level_1A.SetActive(false);
+        Level_1B.SetActive(true);
+        Level_1C.SetActive(true);
+    }
+    void LevelShow_3()
+    {
+        Level_1B.SetActive(false);
+        Level_1C.SetActive(true);
+        Level_1D.SetActive(true);
+    }
+    void LevelShow_4()
+    {
+        Level_1C.SetActive(false);
+        Level_1D.SetActive(true);
+        Level_1E.SetActive(true);
+    }
+    void LevelShow_5()
+    {
+        Level_1D.SetActive(false);
+        Level_1E.SetActive(true);
+        Pyre_A.SetActive(true);
+    }
+    void LevelShow_6()
+    {
+        Level_1E.SetActive(false);
+        Pyre_A.SetActive(true);
+        Pyre_B.SetActive(true);
+    }
+    void LevelShow_7()
+    {
+        Pyre_A.SetActive(false);
     }
 }
